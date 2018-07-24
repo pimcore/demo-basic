6518176bf050e5b2aa040fcd19a3a6b455999631
Christian Fasching <christian.fasching@elements.at>
2018-07-24 17:27:20 +0200

    adapt tests to new setup

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,7 +1,6 @@
 <?php
-define(
-    'PIMCORE_PROJECT_ROOT',
-    realpath(getenv('PIMCORE_PROJECT_ROOT'))
-);
 
-require_once PIMCORE_PROJECT_ROOT . '/pimcore/config/bootstrap.php';
+include __DIR__ . "/../vendor/autoload.php";
+
+\Pimcore\Bootstrap::setProjectRoot();
+\Pimcore\Bootstrap::boostrap();
