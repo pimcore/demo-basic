b2c839f6fdc702f39ab2b467946153ad4484c847
Bernhard Rusch <bernhard.rusch@elements.at>
2019-02-01 15:45:12 +0100

    Date editable just showing format string on frontend - fixes #1667

--- app/Resources/views/Content/default.html.php
+++ app/Resources/views/Content/default.html.php
@@ -9,4 +9,9 @@ $this->extend('layout.html.php');
 
 ?>
 
+<?= $this->date("date", [
+    "format" => "d m Y",
+    'outputFormat' => "%d.%m.%Y"
+]); ?>
+
 <?= $this->template('Includes/content-default.html.php') ?>
